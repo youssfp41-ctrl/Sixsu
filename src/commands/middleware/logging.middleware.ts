74b9b4a50a278fb706f98b47e095f0538dd2b4d5
✻ Mυິhαׅׄm︩︪ɐิძ 🇰🇷🦢 <maldy7478@gmail.com>
2026-05-23 03:02:04 +0200

    feat: add src/commands/middleware/logging.middleware.ts

--- src/commands/middleware/logging.middleware.ts
+++ src/commands/middleware/logging.middleware.ts
@@ -1,17 +1,12 @@
-import { PipelineMiddleware } from "../types/ICommand";
+import { MiddlewareFn } from "../../middleware/types/IMiddleware";
 
-export const loggingMiddleware: PipelineMiddleware = async (
-  ctx,
-  command,
-  next
-) => {
+export const loggingMiddleware: MiddlewareFn = async (ctx, command, next) => {
   const start = Date.now();
   console.log(
-    `[Command] "${command.name}" | user: ${ctx.user.id} | args: [${ctx.args.join(", ")}]`
+    `[Command] "${command?.name}" | user:${ctx.user.id} | args:[${ctx.args.join(", ")}]`
   );
-
   await next();
-
-  const elapsed = Date.now() - start;
-  console.log(`[Command] "${command.name}" completed in ${elapsed}ms`);
+  console.log(
+    `[Command] "${command?.name}" done in ${Date.now() - start}ms`
+  );
 };
