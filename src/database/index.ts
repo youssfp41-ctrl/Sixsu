508460e6778a89faf0e740e60793f76acd596e11
✻ Mυິhαׅׄm︩︪ɐิძ 🇰🇷🦢 <maldy7478@gmail.com>
2026-05-23 05:22:36 +0200

    feat: add src/database/index.ts

---
+++ src/database/index.ts
@@ -0,0 +1,7 @@
+export { DatabaseManager } from "./DatabaseManager";
+export { BaseRepository } from "./repositories/BaseRepository";
+export { UserRepository } from "./repositories/user.repository";
+export type { CreateUserDTO, UpdateUserDTO } from "./repositories/user.repository";
+export { UserModel } from "./models/user.model";
+export type { IUser, UserDocument } from "./models/user.model";
+export type { IRepository } from "./interfaces/IRepository";
